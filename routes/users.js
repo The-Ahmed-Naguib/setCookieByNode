035f511b0817f440710141c7bb529150ff7d59fb
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 14:36:44 +0200

    edit time

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.cookie('3pcookie', 'token', { sameSite: 'none', secure: true , path : '/'});
-  res.set('Set-cookie', 'theCookie=token; SameSite=None; Secure; Domain=dev.siliconexpert.com');
+  res.set('Set-cookie', 'theCookie=token; SameSite=None; Secure; domain=10.68.24.27; Max-Age=86400');
   res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.set('Access-Control-Allow-Credentials', 'true');
   res.send('{"token":"Token"}');
