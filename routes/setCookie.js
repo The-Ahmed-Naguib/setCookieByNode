9f904da5a127ad580627f8a11aee5c9478f51c11
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 22:48:52 +0200

    16

--- routes/setCookie.js
+++ routes/setCookie.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
   let freeUser = null;
   if (req.body['freeUser']) {
     // check the new cookie first
