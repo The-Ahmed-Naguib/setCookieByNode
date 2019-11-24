f66a13136918480921982ef7b75fcedc05c4d8f1
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 14:57:50 +0200

    test 1

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.cookie('3pcookie', 'token', { sameSite: 'none', secure: true , path : '/'});
-  res.set('Set-cookie', 'theCookie=token; SameSite=None; Secure; domain=10.68.24.27;Path=/; Max-Age=86400');
-  res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.set('Set-cookie', 'theCookie=token; domain=10.68.24.27;Path=/; Max-Age=86400');
+  // res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.set('Access-Control-Allow-Credentials', 'true');
   res.send('{"token":"Token"}');
 });
