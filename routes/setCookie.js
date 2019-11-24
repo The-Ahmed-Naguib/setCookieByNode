bb50f24ef6b12cb5b236d17bc2db199476b6da06
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 19:53:51 +0200

    3

--- routes/setCookie.js
+++ routes/setCookie.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(req, res, next) {
-  console.log("********************");
-  console.log(req.headers);
-  console.log("********************");
-  console.log(req.body);
+router.post('/', function (req, res, next) {
+    console.log("********************");
+    console.log(req.headers);
+    console.log("********************");
+    console.log(req.body);
 
-  let freeUser = null;
-  if (req.body['freeUser']) {
-    // check the new cookie first
-    freeUser = req.body['freeUser'];
-  }
+    let freeUser = null;
+    if (req.body['freeUser']) {
+        // check the new cookie first
+        freeUser = req.body['freeUser'];
+    }
+    res.cookie('3pcookie', 'value', {
+        sameSite: 'none',
+        secure: true,
+        domain: '.siliconexpert.com',
+        Path: '/',
+        'Max-Age': 86400
+    });
 
-  res.set('Set-cookie', 'freeUser='+freeUser+';  Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
-  res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.set('Access-Control-Allow-Credentials', 'true');
-  res.send('{"token":"'+freeUser+'"}');
+    // res.set('Set-cookie', 'freeUser=' + freeUser + ';  Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.send('{"token":"' + freeUser + '"}');
 });
 
 
