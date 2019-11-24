0d6351fde2cd8f046419e94970f713c36d0e19f1
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 21:13:57 +0200

    6

--- routes/setCookie.js
+++ routes/setCookie.js
@@ -13,8 +13,10 @@ router.post('/', function (req, res, next) {
         freeUser = req.body['freeUser'];
     }
   res.set('Set-cookie', 'freeUser=' + freeUser + '; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
-  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.set('Access-Control-Allow-Credentials', 'true');
+  res.set('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
   res.send('{"token":"' + freeUser + '"}');
 });
 
