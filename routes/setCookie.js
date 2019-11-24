7387567f056cecfd36871666168e85290be365a7
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 19:27:10 +0200

    2

---
+++ routes/setCookie.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+  console.log("********************");
+  console.log(req.headers);
+  console.log("********************");
+  console.log(req.body);
+
+  let freeUser = null;
+  if (req.body['freeUser']) {
+    // check the new cookie first
+    freeUser = req.body['freeUser'];
+  }
+
+  res.set('Set-cookie', 'freeUser='+freeUser+';  Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
+  res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.set('Access-Control-Allow-Credentials', 'true');
+  res.send('{"token":"'+freeUser+'"}');
+});
+
+
+module.exports = router;
