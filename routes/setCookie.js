bafc909ec3bcf9bd2cb3e0b7d368c8d24a1b7915
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 21:21:59 +0200

    8

--- routes/setCookie.js
+++ routes/setCookie.js
@@ -12,13 +12,13 @@ router.post('/', function (req, res, next) {
         // check the new cookie first
         freeUser = req.body['freeUser'];
     }
-  // res.set('Set-cookie', 'freeUser=' + freeUser + '; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
-  res.set('Set-cookie', 'freeUser=123; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
-  res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.set('Access-Control-Allow-Credentials', 'true');
-  res.set('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.set('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-  res.send('{"token":"123"}');
+    // res.set('Set-cookie', 'freeUser=' + freeUser + '; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
+    res.set('Set-cookie', 'freeUser=123; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    res.send('{"token":"123"}');
 });
 
 
