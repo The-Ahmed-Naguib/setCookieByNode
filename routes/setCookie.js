10e817f078e3216b358c5d2251735f53cdc760bd
Naguib <ahmed.nagib93@gmail.com>
2019-11-24 22:15:17 +0200

    13

--- routes/setCookie.js
+++ routes/setCookie.js
@@ -7,8 +7,8 @@ router.post('/', function (req, res, next) {
     // check the new cookie first
     freeUser = req.body['freeUser'];
   }
-  // res.set('Set-cookie', 'freeUser=' + freeUser + '; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
-  res.header('Set-cookie', 'freeUser=123; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
+  res.set('Set-cookie', 'freeUser=' + freeUser + ';SameSite=None;Secure;domain=.siliconexpert.com;Path=/;Max-Age=86400');
+  // res.header('Set-cookie', 'freeUser=123; sameSite:none ;Secure;domain=.siliconexpert.com;Path=/; Max-Age=86400');
   res.send('{"token":"123"}');
 });
 
